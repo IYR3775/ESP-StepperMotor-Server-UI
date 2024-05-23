04c0c46a8f91af279d1b552679b2708746dae554
IYR3775 <66664082+IYR3775@users.noreply.github.com>
2024-05-23 23:31:03 +0500

    Revert "add pwa"
    This reverts commit 066a44124ea27d70b9a3185d79c57e12f3f67c38.

--- src/main.js
+++ src/main.js
@@ -43,7 +43,6 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 // toastr
 import VueToastr2 from 'vue-toastr-2'
 import 'vue-toastr-2/dist/vue-toastr-2.min.css'
-import './registerServiceWorker'
 window.toastr = require('toastr')
 Vue.use(VueToastr2)
 
